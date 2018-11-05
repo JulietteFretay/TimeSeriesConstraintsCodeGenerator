f68afeeaf60aa53d7983a712cfaad00ac56e5b31
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2018-11-05 22:25:53 +0100

    Added model : generator + parameters

---
+++ src/model/generator/Generator.java
@@ -0,0 +1,11 @@
+package model.generator;
+
+import model.parameters.DecorationTable;
+import model.parameters.SeedTemplate;
+
+public class Generator {
+
+	public Generator(DecorationTable decorationTable, SeedTemplate seedTemplate){
+		
+	}
+}
