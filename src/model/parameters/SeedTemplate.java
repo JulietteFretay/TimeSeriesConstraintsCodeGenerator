4f68fc35a6f298801de5584c6cb0edd9868b2d27
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2018-11-11 16:32:00 +0100

    Doc + State + Arc

--- src/model/parameters/SeedTemplate.java
+++ src/model/parameters/SeedTemplate.java
@@ -1,5 +1,11 @@
 package model.parameters;
 
-public class SeedTemplate {
+import java.util.ArrayList;
 
+public class SeedTemplate {
+	private String name;
+	private State startingState;
+	private ArrayList<StateOrReturn> states;
+	private ArrayList<Arc> links;
+	private int nodeDistance;
 }
