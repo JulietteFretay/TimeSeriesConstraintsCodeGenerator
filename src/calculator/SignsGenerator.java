21c18ef51b0a5ca436f450b0911f3d17bdbbf69b
vjordan <vincent.jordan@outlook.fr>
2018-11-18 12:57:47 +0100

    ajout méthode transformation time série en signes

---
+++ src/calculator/SignsGenerator.java
@@ -0,0 +1,28 @@
+package calculator;
+
+public class SignsGenerator {
+	
+	int[] timeSerie;
+
+	public SignsGenerator(int[] timeSerie){
+		
+		this.timeSerie = timeSerie;
+	}
+	
+	public String[] getSigns(int[] timeSerie) {
+		
+		int nbElements = timeSerie.length;
+		String[] tabSignes = new String[nbElements - 1];
+		
+		for (int i = 0; i < nbElements - 1; i++) {
+			if (timeSerie[i] == timeSerie[i+1]) {
+				tabSignes[i] = "=";
+			} else if (timeSerie[i] < timeSerie[i+1]) {
+				tabSignes[i] = "<";
+			} else {
+				tabSignes[i] = ">";
+			}
+		}
+		return tabSignes;
+	}
+}
